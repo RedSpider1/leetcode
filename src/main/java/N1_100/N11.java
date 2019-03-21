3ade29e34dba322903a04fe614b24bddc86c3bc6
yasinshaw <yasinshaw@gmail.com>
2019-03-21 16:32:41 +0800

    萤火虫-N11

---
+++ src/main/java/N1_100/N11.java
@@ -0,0 +1,20 @@
+package N1_100;
+
+/**
+ * @author Yasin Shaw
+ * @version v1.0
+ */
+public class N11 {
+    public int maxArea(int[] height) {
+        int maxArea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return maxArea;
+    }
+}
