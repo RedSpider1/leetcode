e3fc9ed075174b3131c27df865a0f9a535b6ae66
yasinshaw <yasinshaw@gmail.com>
2019-03-22 14:33:05 +0800

    萤火虫-N3

---
+++ src/main/java/N1_100/N3.kt
@@ -0,0 +1,27 @@
+package N1_100
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import kotlin.math.max
+
+class Solution {
+    fun lengthOfLongestSubstring(s: String): Int {
+        var maxLength = 0
+        val n = s.length
+        val map = HashMap<Char, Int>()
+        var i = 0
+        for (j in 0 until n) {
+            if (s[j] in map) {
+                i = max(map.get(s[j])!!, i)
+            }
+            maxLength = max(maxLength, j - i + 1)
+            map.put(s[j], j + 1)
+        }
+        return maxLength
+    }
+
+    @Test
+    fun test() {
+        assertEquals(lengthOfLongestSubstring("abcabcbb"), 3)
+    }
+}
